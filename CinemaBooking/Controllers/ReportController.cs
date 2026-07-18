089734ed25e92ba4437f1508161d6911921dcaa9
skyandz <pattralada.p@gmail.com>
2026-07-19 03:19:59 +0700

    Add report feature

---
+++ CinemaBooking/Controllers/ReportController.cs
@@ -0,0 +1,65 @@
+﻿using CinemaBooking.Data;
+using CinemaBooking.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaBooking.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var topMovies = await _context.BookingDetails
+                .AsNoTracking()
+                .GroupBy(bookingDetail => new
+                {
+                    bookingDetail.Schedule.MovieId,
+                    bookingDetail.Schedule.Movie.MovieName
+                })
+                .Select(group => new MovieBookingReport
+                {
+                    MovieName = group.Key.MovieName,
+                    TicketCount = group.Count()
+                })
+                .OrderByDescending(movie => movie.TicketCount)
+                .Take(5)
+                .ToListAsync();
+
+            var viewModel = new ReportViewModel
+            {
+                TotalBookings = await _context.Bookings
+                    .AsNoTracking()
+                    .CountAsync(),
+
+                TotalTickets = await _context.BookingDetails
+                    .AsNoTracking()
+                    .CountAsync(),
+
+                TotalMovies = await _context.Movies
+                    .AsNoTracking()
+                    .CountAsync(),
+
+                TotalCinemas = await _context.Cinemas
+                    .AsNoTracking()
+                    .CountAsync(),
+
+                TotalSchedules = await _context.Schedules
+                    .AsNoTracking()
+                    .CountAsync(),
+
+                LastUpdated = DateTime.Now,
+
+                TopMovies = topMovies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CinemaBooking/ViewModels/ReportViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace CinemaBooking.Models.ViewModels
+{
+    public class ReportViewModel
+    {
+        public int TotalBookings { get; set; }
+
+        public int TotalTickets { get; set; }
+
+        public int TotalMovies { get; set; }
+
+        public int TotalCinemas { get; set; }
+
+        public int TotalSchedules { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public List<MovieBookingReport> TopMovies { get; set; }
+            = new List<MovieBookingReport>();
+    }
+
+    public class MovieBookingReport
+    {
+        public string MovieName { get; set; } = string.Empty;
+
+        public int TicketCount { get; set; }
+    }
+}
