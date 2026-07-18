0d7188a8da4d199effa33fcfa275830482c13ded
skyandz <pattralada.p@gmail.com>
2026-07-19 04:55:42 +0700

    Enhance Movie model and views with date handling

--- CinemaBooking/Models/Movie.cs
+++ CinemaBooking/Models/Movie.cs
@@ -1,13 +1,17 @@
 ﻿namespace CinemaBooking.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Movie
 {
     public int MovieId { get; set; }
 
     public string MovieName { get; set; } = string.Empty;
 
+    [DataType(DataType.Date)]
     public DateTime StartDate { get; set; }
 
+    [DataType(DataType.Date)]
     public DateTime StopDate { get; set; }
 
     // Movie 1 เรื่อง มีได้หลายรอบฉาย
